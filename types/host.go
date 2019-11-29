16a620cc63bc1fa6568ff3da9eca0aef7500410b
anrs <anders.x.hu@gmail.com>
2019-11-29 15:01:01 +0800

    report guest status

--- types/host.go
+++ types/host.go
@@ -1,8 +1,15 @@
 package types
 
+import "fmt"
+
 type HostInfo struct {
 	ID      string
 	Cpu     int
 	Mem     int64
 	Storage int64
+}
+
+func (h HostInfo) String() string {
+	return fmt.Sprintf("%s, cpu: %d, memory: %d, storage: %d",
+		h.ID, h.Cpu, h.Mem, h.Storage)
 }
