2e9d0d44d3cf2dc69d1ba89f12a4ac2506dad744
aceralon <aceralon@live.com>
2021-09-10 14:26:04 +0800

    fix grpc file error

--- grpc/gen/yavirtd_grpc.pb.go
+++ grpc/gen/yavirtd_grpc.pb.go
@@ -234,7 +234,7 @@ type yavirtdRPCCopyToGuestClient struct {
 }
 
 func (x *yavirtdRPCCopyToGuestClient) Send(m *CopyOptions) error {
-	return x.ClientStream.SexndMsg(m)
+	return x.ClientStream.SendMsg(m)
 }
 
 func (x *yavirtdRPCCopyToGuestClient) CloseAndRecv() (*CopyMessage, error) {
