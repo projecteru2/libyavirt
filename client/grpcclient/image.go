617f162b96750700b242f455f9dbce80c94b94cd
aceralon <aceralon@live.com>
2021-12-01 15:34:47 +0800

    implement removeImage

--- client/grpcclient/image.go
+++ client/grpcclient/image.go
@@ -16,3 +16,19 @@ func (c *GRPCClient) PushImage(ctx context.Context, imgName, user string) (strin
 
 	return msg.Err, nil
 }
+
+func (c *GRPCClient) RemoveImage(ctx context.Context, imgName, user string, force, prune bool) (removed []string, err error) {
+	opts := &yavpb.RemoveImageOptions{
+		Image: imgName,
+		User:  user,
+		Force: force,
+		Prune: prune,
+	}
+
+	msg, err := c.client.RemoveImage(ctx, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return msg.Removed, nil
+}
