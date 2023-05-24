1dee46a216dc8c6f4897ebb3be01095d8bc65c1d
YuYang <yyangplus@gmail.com>
2023-05-24 11:06:43 +0800

    add resources to InfoMsg

--- types/host.go
+++ types/host.go
@@ -4,14 +4,15 @@ import "fmt"
 
 // HostInfo .
 type HostInfo struct {
-	ID      string
-	CPU     int
-	Mem     int64
-	Storage int64
+	ID        string
+	CPU       int
+	Mem       int64
+	Storage   int64
+	Resources map[string][]byte
 }
 
 // String .
 func (h HostInfo) String() string {
-	return fmt.Sprintf("%s, cpu: %d, memory: %d, storage: %d",
-		h.ID, h.CPU, h.Mem, h.Storage)
+	return fmt.Sprintf("%s, cpu: %d, memory: %d, storage: %d, resources: %v",
+		h.ID, h.CPU, h.Mem, h.Storage, h.Resources)
 }
